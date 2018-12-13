859873f9159c38ecfd85e4828a7187389b387149
Nicholas Butko <nb@8thwall.com>
2018-12-13 14:33:25 -0800

    r0: Simplify camerapipeline examples.

--- examples/camerapipeline/camerafeed/index.js
+++ examples/camerapipeline/camerafeed/index.js
@@ -1,48 +1,15 @@
 // Copyright (c) 2018 8th Wall, Inc.
 
 const onxrloaded = () => {
-  const canvas = document.getElementById('camerafeed')
-
-  // Update the size of the camera feed canvas to fill the screen.
-  const fillScreenWithCanvas = ({orientation}) => {
-    const ww = window.innerWidth
-    const wh = window.innerHeight
-
-    // Wait for orientation change to take effect before handline resize.
-    if (((orientation == 0 || orientation == 180) && ww > wh)
-      || ((orientation == 90 || orientation == -90) && wh > ww)) {
-      window.requestAnimationFrame(() => fillScreenWithCanvas({orientation}))
-      return
-    }
-
-    // Set the canvas geometry to the new window size.
-    canvas.width = ww * window.devicePixelRatio
-    canvas.height = wh * window.devicePixelRatio
-  }
-
-  // Set the initial canvas geometry.
-  fillScreenWithCanvas({orientation: window.orientation})
-
-  // Add an existing camera pipeline module to draw the camera feed.
-  XR.addCameraPipelineModule(XR.GlTextureRenderer.pipelineModule())
-
-  // Add a custom pipeline module to update the canvas size if the phone is rotated.
-  XR.addCameraPipelineModule({
-    name: 'camerafeed',
-    onDeviceOrientationChange: ({orientation}) => {
-      fillScreenWithCanvas({orientation})
-    },
-  })
+  XR.addCameraPipelineModules([  // Add camera pipeline modules.
+    // Existing pipeline modules.
+    XR.FullWindowCanvas.pipelineModule(),   // Modifies the canvas to fill the window.
+    XR.GlTextureRenderer.pipelineModule(),  // Draws the camera feed.
+  ])
 
   // Request camera permissions and run the camera.
-  XR.run({canvas})
+  XR.run({canvas: document.getElementById('camerafeed')})
 }
 
 // Wait until the XR javascript has loaded before making XR calls.
-window.onload = () => {
-  if (window.XR) {
-    onxrloaded()
-  } else {
-    window.addEventListener('xrloaded', onxrloaded)
-  }
-}
+window.onload = () => {window.XR ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)}
--- examples/camerapipeline/simpleshaders/index.js
+++ examples/camerapipeline/simpleshaders/index.js
@@ -1,130 +1,111 @@
 // Copyright (c) 2018 8th Wall, Inc.
 
-const onxrloaded = () => {
+const fragmentShaders = [  // Define some simple shaders to apply to the camera feed.
+  ` precision mediump float;  // Just the camera feed.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() { gl_FragColor = texture2D(sampler, texUv); }`,
+  ` precision mediump float;  // Color boost.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() {
+      vec4 c = texture2D(sampler, texUv);
+      float y = dot(c.rgb, vec3(0.299, 0.587, 0.114));
+      float u = dot(c.rgb, vec3(-.159, -.331, .5)) * 6.0;
+      float v = dot(c.rgb, vec3(.5, -.419, -.081)) * 3.0;
+      gl_FragColor = vec4(y + 1.4 * v, y - .343 * u - .711 * v, y + 1.765 * u, c.a);
+    }`,
+  ` precision mediump float;  // Vignette.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() {
+      float x = texUv.x - .5;
+      float y = texUv.y - .5;
+      float v = 1.5 - sqrt(x * x + y * y) * 2.5;
+      vec4 c = texture2D(sampler, texUv);
+      gl_FragColor = vec4(c.rgb * (v > 1.0 ? 1.0 : v), c.a);
+    }`,
+  ` precision mediump float;  // Black and white.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() {
+      vec4 c = texture2D(sampler, texUv);
+      gl_FragColor = vec4(vec3(dot(c.rgb, vec3(0.299, 0.587, 0.114))), c.a);
+    }`,
+  ` precision mediump float;  // Sepia.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() {
+      vec4 c = texture2D(sampler, texUv);
+      gl_FragColor.r = dot(c.rgb, vec3(.393, .769, .189));
+      gl_FragColor.g = dot(c.rgb, vec3(.349, .686, .168));
+      gl_FragColor.b = dot(c.rgb, vec3(.272, .534, .131));
+      gl_FragColor.a = c.a;
+    }`,
+  ` precision mediump float;  // Purple.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() {
+      vec4 c = texture2D(sampler, texUv);
+      float y = dot(c.rgb, vec3(0.299, 0.587, 0.114));
+      vec3 p = vec3(.463, .067, .712);
+      vec3 rgb = y < .25 ? (y * 4.0) * p : ((y - .25) * 1.333) * (vec3(1.0, 1.0, 1.0) - p) + p;
+      gl_FragColor = vec4(rgb, c.a);
+    }`,
+]
 
-  const fragmentShaders = [
-    // Just the camera feed.
-    ` precision mediump float;
-      varying vec2 texUv;
-      uniform sampler2D sampler;
-      void main() {
-        gl_FragColor = texture2D(sampler, texUv);
-      }`,
-    // Color boost.
-    ` precision mediump float;
-      varying vec2 texUv;
-      uniform sampler2D sampler;
-      void main() {
-        vec4 c = texture2D(sampler, texUv);
-        float y = dot(c.rgb, vec3(0.299, 0.587, 0.114));
-        float u = dot(c.rgb, vec3(-.159, -.331, .5)) * 6.0;
-        float v = dot(c.rgb, vec3(.5, -.419, -.081)) * 3.0;
-        float r = y + 1.4 * v;
-        float g = y - .343 * u - .711 * v;
-        float b = y + 1.765 * u;
-        gl_FragColor = vec4(r, g, b, c.a);
-      }`,
-    // Vignette.
-    ` precision mediump float;
-      varying vec2 texUv;
-      uniform sampler2D sampler;
-      void main() {
-        float x = texUv.x - .5;
-        float y = texUv.y - .5;
-        float v = 1.5 - sqrt(x * x + y * y) * 2.5;
-        v = v > 1.0 ? 1.0 : v;
-        vec4 c = texture2D(sampler, texUv);
-        gl_FragColor = vec4(c.rgb * v, c.a);
-      }`,
-    // Black and white.
-    ` precision mediump float;
-      varying vec2 texUv;
-      uniform sampler2D sampler;
-      void main() {
-        vec4 c = texture2D(sampler, texUv);
-        gl_FragColor = vec4(vec3(dot(c.rgb, vec3(0.299, 0.587, 0.114))), c.a);
-      }`,
-    // Sepia.
-    ` precision mediump float;
-      varying vec2 texUv;
-      uniform sampler2D sampler;
-      void main() {
-        vec4 c = texture2D(sampler, texUv);
-        gl_FragColor.r = dot(c.rgb, vec3(.393, .769, .189));
-        gl_FragColor.g = dot(c.rgb, vec3(.349, .686, .168));
-        gl_FragColor.b = dot(c.rgb, vec3(.272, .534, .131));
-        gl_FragColor.a = c.a;
-      }`,
-    // Purple.
-    ` precision mediump float;
-      varying vec2 texUv;
-      uniform sampler2D sampler;
-      void main() {
-        vec4 c = texture2D(sampler, texUv);
-        float y = dot(c.rgb, vec3(0.299, 0.587, 0.114));
-        vec3 p = vec3(.463, .067, .712);
-        vec3 p1 = vec3(1.0, 1.0, 1.0) - p;
-        vec3 rgb = y < .25 ? (y * 4.0) * p : ((y - .25) * 1.333) * p1 + p;
-        gl_FragColor = vec4(rgb, c.a);
-      }`,
-  ]
+// Define a custom pipeline module. This module cycles through a set of pre-defined shaders each
+// time the next button is pressed. It also updates the button style on orientation changes.
+const nextbuttonPipelineModule = () => {
+  const nextButton = document.getElementById('nextbutton')
+  let idx = 0  // Index of the shader to use next.
 
-  let idx = 0
   const nextShader = () => {
+    // Reconfigure the texture renderer pipline module to use the next shader.
     XR.GlTextureRenderer.configure({fragmentSource: fragmentShaders[idx]})
     idx = (idx + 1) % fragmentShaders.length
   }
 
-  const canvas = document.getElementById('camerafeed')
+  nextShader()                     // Call 'nextShader' once to set the first shader.
+  nextButton.onclick = nextShader  // Switch to the next shader when the next button is pressed.
 
-  const nextButton = document.getElementById('nextbutton')
-  nextButton.onclick = nextShader
-
-  // Update the size of the camera feed canvas to fill the screen.
-  const fillScreenWithCanvas = ({orientation}) => {
+  const adjustButtonTextCenter = ({orientation}) => { // Update the line height on the button.
     const ww = window.innerWidth
     const wh = window.innerHeight
 
     // Wait for orientation change to take effect before handline resize.
     if (((orientation == 0 || orientation == 180) && ww > wh)
       || ((orientation == 90 || orientation == -90) && wh > ww)) {
-      window.requestAnimationFrame(() => fillScreenWithCanvas({orientation}))
+      window.requestAnimationFrame(() => adjustButtonTextCenter({orientation}))
       return
     }
 
-    // Set the canvas geometry to the new window size.
-    canvas.width = ww * window.devicePixelRatio
-    canvas.height = wh * window.devicePixelRatio
     nextButton.style.lineHeight = `${nextButton.getBoundingClientRect().height}px`
   }
 
-  // Set the initial canvas geometry.
-  fillScreenWithCanvas({orientation: window.orientation})
-
-  // Add an existing camera pipeline module to draw the camera feed.
-  XR.addCameraPipelineModule(XR.GlTextureRenderer.pipelineModule())
-
-  nextShader()
-
-  // Add a custom pipeline module to update the canvas size if the phone is rotated.
-  XR.addCameraPipelineModule({
-    name: 'camerafeed',
-    onDeviceOrientationChange: ({orientation}) => {
-      fillScreenWithCanvas({orientation})
+  // Return a pipeline module that updates the state of the UI on relevant lifecycle events.
+  return {
+    name: 'nextbutton',
+    onStart: ({orientation}) => {
+      nextButton.style.visibility = 'visible'
+      adjustButtonTextCenter({orientation})
     },
-  })
+    onDeviceOrientationChange: ({orientation}) => { adjustButtonTextCenter({orientation}) },
+  }
+}
+
+const onxrloaded = () => {
+  XR.addCameraPipelineModules([  // Add camera pipeline modules.
+    // Existing pipeline modules.
+    XR.FullWindowCanvas.pipelineModule(),   // Modifies the canvas to fill the window.
+    XR.GlTextureRenderer.pipelineModule(),  // Draws the camera feed.
+    // Custom pipeline modules.
+    nextbuttonPipelineModule(),             // Cycles through shaders and keeps UI up to date.
+  ])
 
   // Request camera permissions and run the camera.
-  XR.run({canvas})
+  XR.run({canvas: document.getElementById('camerafeed')})
 }
 
 // Wait until the XR javascript has loaded before making XR calls.
-window.onload = () => {
-  const nextButton = document.getElementById('nextbutton')
-  nextButton.style.lineHeight = `${nextButton.getBoundingClientRect().height}px`
-  if (window.XR) {
-    onxrloaded()
-  } else {
-    window.addEventListener('xrloaded', onxrloaded)
-  }
-}
+window.onload = () => {window.XR ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)}
