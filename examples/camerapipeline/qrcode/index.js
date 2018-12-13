aa0629ea12d997012eaf302caf139f7926b08373
Nicholas Butko [8th Wall] <nb@8thwall.com>
2018-12-13 14:34:57 -0800

    Simplify camerapipeline examples.

--- examples/camerapipeline/qrcode/index.js
+++ examples/camerapipeline/qrcode/index.js
@@ -1,110 +1,77 @@
 // Copyright (c) 2018 8th Wall, Inc.
 
-const onxrloaded = () => {
-  const url = document.getElementById('url')
-  const canvas = document.getElementById('camerafeed')
-  url.lastSeen = 0
-
-  // Update the size of the camera feed canvas to fill the screen.
-  const fillScreenWithCanvas = ({orientation}) => {
-    const ww = window.innerWidth
-    const wh = window.innerHeight
-
-    // Wait for orientation change to take effect before handline resize.
-    if (((orientation == 0 || orientation == 180) && ww > wh)
-      || ((orientation == 90 || orientation == -90) && wh > ww)) {
-      window.requestAnimationFrame(() => fillScreenWithCanvas({orientation}))
-      return
-    }
-
-    // Set the canvas geometry to the new window size.
-    canvas.width = ww * window.devicePixelRatio
-    canvas.height = wh * window.devicePixelRatio
-  }
-
-  // Set the initial canvas geometry.
-  fillScreenWithCanvas({orientation: window.orientation})
-
-  // Add an existing camera pipeline module to draw the camera feed.
-  XR.addCameraPipelineModule(XR.GlTextureRenderer.pipelineModule())
-
-  // Add a custom pipeline module to access a UInt8Array with a black-and-white reduced resolution
-  // copy of the image feed in the onProcessCpu camera lifecycle method.
-  XR.addCameraPipelineModule(
-    XR.CameraPixelArray.pipelineModule({ luminance: true, maxDimension: 640 }))
-
-  // Add a custom pipeline module to detect QR codes and update the display.
-  XR.addCameraPipelineModule({
-    name: 'scanresult',
-
+// Define a custom pipeline module. This module scans the camera feed for qr codes, and makes the
+// result available to other modules in onUpdate. It requires that the CameraPixelArray module is
+// installed and configured to provide a luminance (black and white) image.
+const qrprocessPipelineModule = () => {
+  return {
+    name: 'qrprocess',
     onProcessCpu: ({processGpuResult}) => {
       // Check whether there is any data ready to process.
-      const {camerapixelarray} = processGpuResult
-      if (!camerapixelarray || !camerapixelarray.pixels) {
-        return
+      if (!processGpuResult.camerapixelarray || !processGpuResult.camerapixelarray.pixels) {
+        return {found: false}
       }
 
-      // Get pointers to the pixel data and image geometry from the CameraPixelArray camera pipeline
-      // module.
-      const { rows, cols, pixels } = camerapixelarray
-
-      // Set input variables on the global qrcode object before calling process.
-      window.qrcode.width = cols
-      window.qrcode.height = rows
-      window.qrcode.grayscale = () => {return pixels}
-
-      let foundText
       try {
-        // Scan the image for a QR code.
-        foundText = window.qrcode.process()
+        // Set input variables on the global qrcode object before calling qrcode.process().
+        window.qrcode.width = processGpuResult.camerapixelarray.cols
+        window.qrcode.height = processGpuResult.camerapixelarray.rows
+        window.qrcode.grayscale = () => { return processGpuResult.camerapixelarray.pixels }
+        return {found: true, foundText: window.qrcode.process()}  // Scan the image for a QR code.
       } catch (e) {
-        // jsqrcode throws erros when qr codes are not found in an image.
+        return {found: false}  // jsqrcode throws errors when qr codes are not found in an image.
       }
-
-      return foundText ? {found: true, foundText} : {found: false}
     },
+  }
+}
 
+// Define a custom pipeline module. This module updates UI elements with the result of the QR code
+// scanning, and navigates to the found url on any tap to the screen.
+const qrdisplayPipelineModule = () => {
+  const url = document.getElementById('url')
+  let lastSeen = 0
+
+  // if the window is touched anywhere, navigate to the URL that was detected.
+  window.addEventListener('touchstart', () => {
+    if (!url.href || !url.href.startsWith('http')) { return }
+    XR.pause()
+    url.innerHTML = `Navigating to ${url.href}`
+    window.location.href = url.href
+  })
+
+  return {
+    name: 'qrdisplay',
+    onStart: () => { url.style.visibility = 'visible' },  // Show the card that displays the url.
     onUpdate: ({processCpuResult}) => {
-      const {scanresult} = processCpuResult
-      if (!scanresult) {
-        return
-      }
+      if (!processCpuResult.qrprocess) { return }
 
-      if (scanresult.found) {
-        url.innerHTML = `<u>${scanresult.foundText}</u>`
-        url.href = scanresult.foundText
-        url.lastSeen = Date.now()
-      } else if (Date.now() - url.lastSeen > 5000) {
+      // Toggle display text based on whether a qrcode result was found.
+      if (processCpuResult.qrprocess.found) {
+        url.innerHTML = `<u>${processCpuResult.qrprocess.foundText}</u>`
+        url.href = processCpuResult.qrprocess.foundText
+        lastSeen = Date.now()
+      } else if (Date.now() - lastSeen > 5000) {
         url.innerHTML = 'Scanning for QR Code...'
         url.href = ''
       }
     },
-
-    onDeviceOrientationChange: ({orientation}) => {
-      fillScreenWithCanvas({orientation})
-    },
-  })
-
-  const navigateToFoundUrl = () => {
-    if (url.href && url.href.startsWith('http')) {
-      XR.pause()
-      url.innerHTML = `Navigating to ${url.href}`
-      window.location.href = url.href
-    }
   }
+}
 
-  // Navigate to the URL that was detected.
-  window.addEventListener('touchstart', navigateToFoundUrl)
+const onxrloaded = () => {
+  XR.addCameraPipelineModules([  // Add camera pipeline modules.
+    // Existing pipeline modules.
+    XR.FullWindowCanvas.pipelineModule(),   // Modifies the canvas to fill the window.
+    XR.GlTextureRenderer.pipelineModule(),  // Draws the camera feed.
+    XR.CameraPixelArray.pipelineModule({ luminance: true, maxDimension: 640 }),  // Provides pixels.
+    // Custom pipeline modules.
+    qrprocessPipelineModule(),              // Scans the image for QR Codes
+    qrdisplayPipelineModule(),              // Displays the result of QR Code scanning.
+  ])
 
   // Request camera permissions and run the camera.
-  XR.run({canvas})
+  XR.run({canvas: document.getElementById('camerafeed')})
 }
 
 // Wait until the XR javascript has loaded before making XR calls.
-window.onload = () => {
-  if (window.XR) {
-    onxrloaded()
-  } else {
-    window.addEventListener('xrloaded', onxrloaded)
-  }
-}
+window.onload = () => {window.XR ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)}
