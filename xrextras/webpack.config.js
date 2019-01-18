cf07a52dc05681371f9dad2fff7449cc092cfb1e
Nicholas Butko <nb@8thwall.com>
2019-01-18 15:37:34 -0800

    r1: Clean up some examples.

--- xrextras/webpack.config.js
+++ xrextras/webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path')
 
 module.exports = {
-  // mode: 'development',
-  // devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
     filename: 'xrextras.js',
