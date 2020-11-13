7bdcb00cbbadd1ad42bf8d3e9bdae97e58684483
Tony Tomarchio <tony@tomarchio.cc>
2020-11-13 12:11:12 -0800

    r0: Fix BabylonJS examples

--- examples/babylonjs/placeground/index.js
+++ examples/babylonjs/placeground/index.js
@@ -1,34 +1,39 @@
-const modelRootURL = './'                                 // Directory where 3D model lives
-const modelFile = 'tree.glb'                              // 3D model to spawn at tap
-const startScale = new BABYLON.Vector3(0.01, 0.01, 0.01)  // Initial scale value for our model
-const endScale = new BABYLON.Vector3(0.05, 0.05, 0.05)    // Ending scale value for our model
-const animationMillis = 750                               // Animate over 0.75 seconds
+/* globals BABYLON TWEEN XR8 XRExtras */
+
+const modelRootURL = './'                               // Directory where 3D model lives
+const modelFile = 'tree.glb'                            // 3D model to spawn at tap
+const startScale = new BABYLON.Vector3(0.1, 0.1, -0.1)  // Initial scale value for our model
+const endScale = new BABYLON.Vector3(2.0, 2.0, -2.0)    // Ending scale value for our model
+const animationMillis = 750                             // Animate over 0.75 seconds
 
 let surface, engine, scene, camera
 
 // Populates some object into an XR scene and sets the initial camera position.
-const initXrScene = ({ scene, camera }) => {
-  console.log('initXrScene')
-
-  const directionalLight = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(0, -1, 1), scene)
+const initXrScene = () => {
+  const directionalLight = new BABYLON.DirectionalLight(
+    'DirectionalLight',
+    new BABYLON.Vector3(0, -1, 1),
+    scene
+  )
   directionalLight.intensity = 1.0
 
   const ground = BABYLON.Mesh.CreatePlane('ground', 100, scene)
   ground.rotation.x = Math.PI / 2
-  ground.material = new BABYLON.StandardMaterial("groundMaterial", scene)
+  ground.material = new BABYLON.StandardMaterial('groundMaterial', scene)
+  ground.material.diffuseColor = BABYLON.Color3.Purple()
   ground.material.alpha = 0
   surface = ground
 
   // Set the initial camera position relative to the scene we just laid out. This must be at a
   // height greater than y=0.
-  camera.position = new BABYLON.Vector3(0,3,5)
+  camera.position = new BABYLON.Vector3(0, 3, -5)
 }
 
 const placeObjectTouchHandler = (e) => {
   // console.log('placeObjectTouchHandler')
   // Call XrController.recenter() when the canvas is tapped with two fingers. This resets the
   // AR camera to the position specified by XrController.updateCameraProjectionMatrix() above.
-  if (e.touches.length == 2) {
+  if (e.touches.length === 2) {
     XR8.XrController.recenter()
   }
 
@@ -37,46 +42,38 @@ const placeObjectTouchHandler = (e) => {
   }
 
   // If the canvas is tapped with one finger and hits the "surface", spawn an object.
-
   const pickResult = scene.pick(e.touches[0].clientX, e.touches[0].clientY)
-  if (pickResult.hit && pickResult.pickedMesh == surface) {
-
-    const gltf = BABYLON.SceneLoader.LoadAssetContainer(
+  if (pickResult.hit && pickResult.pickedMesh === surface) {
+    BABYLON.SceneLoader.ImportMesh(
+      '',
       modelRootURL,
       modelFile,
       scene,
-      function (container) {  // onSuccess
-        const scale = Object.assign({}, startScale)
-        const yRot = Math.random() * 360
-        for (i = 0; i < container.meshes.length; i++) {
-          container.meshes[i]._position.x = pickResult.pickedPoint.x
-          container.meshes[i]._position.z = pickResult.pickedPoint.z
-          container.meshes[i]._rotation.y = yRot
-          container.meshes[i]._scaling.x = scale.x
-          container.meshes[i]._scaling.y = scale.y
-          container.meshes[i]._scaling.z = scale.z
-        }
-        // Adds all elements to the scene
-        container.addAllToScene()
+      (newMeshes) => {  // onSuccess
+        const mesh = newMeshes[0]
+        mesh.scaling = new BABYLON.Vector3(startScale.x, startScale.y, startScale.z)
 
+        const yRot = Math.random() * Math.PI
+        mesh.position = new BABYLON.Vector3(pickResult.pickedPoint.x, 0, pickResult.pickedPoint.z)
+        mesh.rotation = new BABYLON.Vector3(0, yRot, 0)
+
+        const scale = Object.assign({}, startScale)
         new TWEEN.Tween(scale)
-        .to(endScale, animationMillis)
-        .easing(TWEEN.Easing.Elastic.Out) // Use an easing function to make the animation smooth.
-        .onUpdate(() => {
-          for (i = 0; i < container.meshes.length; i++) {
-            container.meshes[i]._scaling.x = scale.x
-            container.meshes[i]._scaling.y = scale.y
-            container.meshes[i]._scaling.z = scale.z
-          }
-        })
-        .start() // Start the tween immediately.
+          .to(endScale, animationMillis)
+          .easing(TWEEN.Easing.Elastic.Out)  // Use an easing function to make the animation smooth.
+          .onUpdate(() => {
+            mesh.scaling.x = scale.x
+            mesh.scaling.y = scale.y
+            mesh.scaling.z = scale.z
+          })
+          .start()  // Start the tween immediately.
       },
-      function (xhr) { //onProgress
-        console.log(`${(xhr.loaded / xhr.total * 100 )}% loaded`)
+      (xhr) => {  // onProgress
+        console.log(`${(xhr.loaded / xhr.total * 100)}% loaded`)
       },
-      function (error) { //onError
+      () => {  // onError
         console.log('Error loading model')
-      },
+      }
     )
   }
 }
@@ -84,13 +81,13 @@ const placeObjectTouchHandler = (e) => {
 const startScene = () => {
   const canvas = document.getElementById('renderCanvas')
 
-  engine = new BABYLON.Engine(canvas, true, { stencil: true, preserveDrawingBuffer: true }, true)
+  engine = new BABYLON.Engine(canvas, true, {stencil: true, preserveDrawingBuffer: true})
   engine.enableOfflineSupport = false
 
   scene = new BABYLON.Scene(engine)
   camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, 0), scene)
 
-  initXrScene({ scene, camera }) // Add objects to the scene and set starting camera position.
+  initXrScene()  // Add objects to the scene and set starting camera position.
 
   // Connect the camera to the XR engine and show camera feed
   camera.addBehavior(XR8.Babylonjs.xrCameraBehavior(), true)
@@ -109,11 +106,10 @@ const startScene = () => {
 }
 
 const onxrloaded = () => {
-  XR8.addCameraPipelineModules([  // Add camera pipeline modules.
-    XRExtras.AlmostThere.pipelineModule(),       // Detects unsupported browsers and gives hints.
-    XRExtras.FullWindowCanvas.pipelineModule(),  // Modifies the canvas to fill the window.
-    XRExtras.Loading.pipelineModule(),           // Manages the loading screen on startup.
-    XRExtras.RuntimeError.pipelineModule(),      // Shows an error image on runtime error.
+  XR8.addCameraPipelineModules([             // Add camera pipeline modules.
+    XRExtras.AlmostThere.pipelineModule(),   // Detects unsupported browsers and gives hints.
+    XRExtras.Loading.pipelineModule(),       // Manages the loading screen on startup.
+    XRExtras.RuntimeError.pipelineModule(),  // Shows an error image on runtime error.
   ])
 
   startScene()
@@ -121,4 +117,10 @@ const onxrloaded = () => {
 
 // Show loading screen before the full XR library has been loaded.
 const load = () => { XRExtras.Loading.showLoading({onxrloaded}) }
-window.onload = () => { window.XRExtras ? load() : window.addEventListener('xrextrasloaded', load) }
+window.onload = () => {
+  if (window.XRExtras) {
+    load()
+  } else {
+    window.addEventListener('xrextrasloaded', load)
+  }
+}
