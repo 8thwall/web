e19b94d5fecfcc0fad7026b9c246184004962c8c
Tony Tomarchio <tony@tomarchio.cc>
2021-06-07 16:25:27 -0700

    Update three.js custom pipeline module example

--- examples/threejs/custom-pipeline-module/customThreejsPipelineModule.js
+++ examples/threejs/custom-pipeline-module/customThreejsPipelineModule.js
@@ -1,44 +1,65 @@
 const customThreejsPipelineModule = () => {
   let scene3
+  let engaged = false
 
+  const engage = ({canvas, canvasWidth, canvasHeight, GLctx}) => {
+    if (engaged) {
+      return
+    }
+    const scene = new window.THREE.Scene()
+    const camera = new window.THREE.PerspectiveCamera(
+      60.0, /* initial field of view; will get set based on device info later. */
+      canvasWidth / canvasHeight,
+      0.01,
+      1000.0,
+    )
+    scene.add(camera)
+
+    const renderer = new window.THREE.WebGLRenderer({
+      canvas,
+      context: GLctx,
+      alpha: false,
+      antialias: true,
+    })
+    renderer.autoClear = false
+    renderer.setSize(canvasWidth, canvasHeight)
+
+    scene3 = {scene, camera, renderer}
+    engaged = true
+  }
   return {
     name: 'customthreejs',
-    onStart: ({canvas, canvasWidth, canvasHeight, GLctx}) => {
-      const scene = new window.THREE.Scene()
-      const camera = new window.THREE.PerspectiveCamera(
-        60.0,  /* initial field of view; will get set based on device info later. */
-        canvasWidth / canvasHeight,
-        0.01,
-        1000.0,
-      )
-      scene.add(camera)
-
-      const renderer = new window.THREE.WebGLRenderer({
-        canvas,
-        context: GLctx,
-        alpha: false,
-        antialias: true,
-      })
-      renderer.autoClear = false
-      renderer.setSize(canvasWidth, canvasHeight)
-
-      scene3 = {scene, camera, renderer}
-    },
+    onStart: (args) => engage(args),
+    onAttach: (args) => engage(args),
+    onDetach: () => { engaged = false },
     onUpdate: ({processCpuResult}) => {
-      if (!processCpuResult.reality) {
+      const realitySource = processCpuResult.reality || processCpuResult.facecontroller
+
+      if (!realitySource) {
         return
       }
-      const {rotation, position, intrinsics} = processCpuResult.reality
+
+      const {rotation, position, intrinsics} = realitySource
       const {camera} = scene3
 
       for (let i = 0; i < 16; i++) {
         camera.projectionMatrix.elements[i] = intrinsics[i]
       }
-      // Fix for broken raycasting in r103 and higher. Related to https://github.com/mrdoob/three.js/pull/15996
-      // Note: camera.projectionMatrixInverse wasn't introduced until r96 so check before calling getInverse()
+
+      // Fix for broken raycasting in r103 and higher. Related to:
+      //   https://github.com/mrdoob/three.js/pull/15996
+      // Note: camera.projectionMatrixInverse wasn't introduced until r96 so check before setting
+      // the inverse
       if (camera.projectionMatrixInverse) {
-        camera.projectionMatrixInverse.copy(camera.projectionMatrix).invert()
+        if (camera.projectionMatrixInverse.invert) {
+          // THREE 123 preferred version
+          camera.projectionMatrixInverse.copy(camera.projectionMatrix).invert()
+        } else {
+          // Backwards compatible version
+          camera.projectionMatrixInverse.getInverse(camera.projectionMatrix)
+        }
       }
+
       if (rotation) {
         camera.setRotationFromQuaternion(rotation)
       }
@@ -47,6 +68,9 @@ const customThreejsPipelineModule = () => {
       }
     },
     onCanvasSizeChange: ({canvasWidth, canvasHeight}) => {
+      if (!engaged) {
+        return
+      }
       const {renderer} = scene3
       renderer.setSize(canvasWidth, canvasHeight)
     },
